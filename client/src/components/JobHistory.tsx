fa4267ad8861b31e02347fd74e869c075f91fe2c
Trevor <trevorhilz@gmail.com>
2024-10-22 13:47:26 -0500

    draft to push to main

--- client/src/components/JobHistory.tsx
+++ client/src/components/JobHistory.tsx
@@ -1,56 +1,45 @@
 const JobHistory = () => {
   return (
-    <div className='ml-8 list-none '>
-      <div className='flex flex-col justify-center'>
-        <div className='text-xl font-bold underline'>
-          Software Engineer | 2023 - 2024
-        </div>
-        <li className='ml-8'>
-          {' '}
-          Binary Artisan (Contract) | 2024 – 2024 |{' '}
-          <a
-            href='https://www.countyourcards.com/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='underline'
-          >
-            Count Your Cards
-          </a>
-        </li>
-        <li className='ml-8 font-bold underline'>
-          Open Source Project | 2023 – 2024
-        </li>
-        <li className='ml-12'>
-          {' '}
-          Nemo | Kubernetes Monitoring Tool |{' '}
-          <a
-            href='https://www.nemomonitoring.com/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='underline'
-          >
-            Nemo Monitoring
-          </a>
-        </li>
-        <li className='ml-12'> Tech Times | New Aggregation Site</li>
-        <li className='ml-12'> Binary Bridge | Job Posting Aggregator</li>
-        <li className='ml-12'> Paper Trading | Stock Trading Simulator</li>
+    <div className='flex flex-col items-center list-none '>
+      <div className='text-xl font-bold underline'>
+        Software Engineer | 2023 - 2024
       </div>
-      <br></br>
-      <header className='text-xl font-bold underline'>
+      <li>
+        Binary Artisan (Contract) | 2024 – 2024 |{' '}
+        <a
+          href='https://www.countyourcards.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline'
+        >
+          Count Your Cards
+        </a>
+      </li>
+      <li className='mt-4 text-lg font-bold underline'>
+        Open Source Project | 2023 – 2024
+      </li>
+      <li>
+        {' '}
+        Nemo | Kubernetes Monitoring Tool |{' '}
+        <a
+          href='https://www.nemomonitoring.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline'
+        >
+          Nemo Monitoring
+        </a>
+      </li>
+      <li> Tech Times | New Aggregation Site</li>
+      <li> Binary Bridge | Job Posting Aggregator</li>
+      <li> Paper Trading | Stock Trading Simulator</li>
+
+      <header className='mt-4 text-xl font-bold underline'>
         Wealth Experience | 2014 - 2023
       </header>
-      <li className='ml-8 list-none'>Texas Capital Bank</li>
-      <li className='ml-8 list-none'>J P Morgan Chase</li>
-      <li className='ml-8 list-none'>Fidelity Investments</li>
-      {/* <li className='ml-8 list-none'>
-        Analyzed financial data using Bloomberg and Morningstar, supporting
-        portfolio management for assets worth $140M
-      </li>
-      <li className='ml-8 list-none'>
-        Automated reporting processes using advanced Excel scripts, reducing
-        preparation time by 40% and improving data presentation
-      </li> */}
+      <li>Texas Capital Bank | Executive Director</li>
+      <li>J P Morgan Chase | Vice President </li>
+      <li>Fidelity Investments</li>
     </div>
   );
 };
